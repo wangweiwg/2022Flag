a572089671e4f463548a7826ceb6f351bc3d1d98
wangwei <wangwei641@163.com>
2022-03-18 16:00:40 +0800

    CSS module 支持和 sass

--- code/backend/client/src/App.js
+++ code/backend/client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
 import Menus from './components/Menus/index';
 
 function App() {
   return (
-    <div className="app">
-      <header className='header'>头部</header>
-      <footer className='footer'>
+    <div className={styles.app}>
+      <header className={styles.header}>头部</header>
+      <footer className={styles.footer}>
         <Menus />
-        <div className='container'>内容部分</div>
+        <div className={styles.container}>内容部分</div>
       </footer>
     </div>
   );
--- code/backend/client/src/components/Menus/index.js
+++ code/backend/client/src/components/Menus/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Menu } from 'antd';
 import {
   DesktopOutlined,
   PieChartOutlined,
 } from '@ant-design/icons';
+import styles from './index.module.css';
 
 // const { SubMenu } = Menu;
 
@@ -11,21 +12,19 @@ class Menus extends React.Component {
 
   render() {
     return (
-      <Layout>
-          <Menu defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-             店铺信息
-            </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />}>
-              会员信息
-            </Menu.Item>
-            {/* <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu> */}
-          </Menu>
-      </Layout>
+      <Menu className={styles.menu} defaultSelectedKeys={['1']} mode="inline">
+        <Menu.Item key="1" icon={<PieChartOutlined />}>
+        店铺信息
+        </Menu.Item>
+        <Menu.Item key="2" icon={<DesktopOutlined />}>
+          会员信息
+        </Menu.Item>
+        {/* <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+          <Menu.Item key="3">Tom</Menu.Item>
+          <Menu.Item key="4">Bill</Menu.Item>
+          <Menu.Item key="5">Alex</Menu.Item>
+        </SubMenu> */}
+      </Menu>
     );
   }
 }
